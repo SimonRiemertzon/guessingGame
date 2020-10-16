8198f6a47e810a8f2d43cb0492b7a671ba02cff7
Siniom <simon.riemertzon@gmail.com>
2020-10-17 00:10:39 +0200

    Add starting files

---
+++ src/com/developer/yrgo/siniom/Guesser.java
@@ -0,0 +1,5 @@
+package com.developer.yrgo.siniom;
+
+public class Guesser {
+    
+}
---
+++ src/com/developer/yrgo/siniom/Main.java
@@ -0,0 +1,8 @@
+package com.developer.yrgo.siniom;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //something something
+    }
+}
