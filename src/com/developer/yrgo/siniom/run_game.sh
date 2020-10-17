8e0bcda29a3f70f5966cba8970447424852eb6a4
simonriemertzon <simon.riemetzon@gmail.com>
2020-10-18 00:59:17 +0200

    Change script variable to accomodate filenamechange

--- src/com/developer/yrgo/siniom/run_game.sh
+++ src/com/developer/yrgo/siniom/run_game.sh
@@ -1,17 +1,18 @@
 NAME="Riemertzon_Simon"
 SUFFIX="_labb"
 DIRPATH=$NAME$SUFFIX
-PACKAGE="com/developer/yrgo/siniom"
+PACKAGE="/src/com/developer/yrgo/siniom"
+PROJECTNAME=GuessingGame
 echo $NAME
 mkdir -p $DIRPATH/$PACKAGE
-cp *.java $DIRPATH/$PACKAGE
+cp -r *.java $DIRPATH/$PACKAGE
 cd $DIRPATH
 echo -n "Running game from: "
 pwd
 echo "Compiling..."
-javac $PACKAGE/Main.java
+javac $PACKAGE/$PROJECTNAME.java
 echo "Running..."
-java $PACKAGE/Main
+java $PACKAGE/$PROJECTNAME
 echo "Done!"
 echo "Removing class files"
 rm -r $PACKAGE/*.class
