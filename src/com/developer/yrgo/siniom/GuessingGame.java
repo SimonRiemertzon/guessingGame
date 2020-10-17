7f7f43a6dbf663464bf702d28f6b386e965ca297
simonriemertzon <simon.riemetzon@gmail.com>
2020-10-17 20:23:21 +0200

    Rename files according to task-requirements

--- src/com/developer/yrgo/siniom/GuessingGame.java
+++ src/com/developer/yrgo/siniom/GuessingGame.java
@@ -1,6 +1,6 @@
 package com.developer.yrgo.siniom;
 
-public class Main {
+public class GuessingGame {
     public static void main(String[] args) {
         Guesser guesser = new Guesser(0, 1000);
         guesser.start();
