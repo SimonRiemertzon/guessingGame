d7e262366128891920db33f92328e76dd0e08266
simonriemertzon <simon.riemetzon@gmail.com>
2020-10-17 19:18:36 +0200

    Finish script that uses accurate package

--- src/com/developer/yrgo/siniom/run_game.sh
+++ src/com/developer/yrgo/siniom/run_game.sh
@@ -1,12 +1,21 @@
-NAME="RIEMERTZON_SIMON"
+NAME="Riemertzon_Simon"
 SUFFIX="_labb"
 DIRPATH=$NAME$SUFFIX
+PACKAGE="com/developer/yrgo/siniom"
 echo $NAME
-mkdir $DIRPATH
-cp *.java $DIRPATH
+mkdir -p $DIRPATH/$PACKAGE
+cp *.java $DIRPATH/$PACKAGE
 cd $DIRPATH
 echo -n "Running game from: "
 pwd
+echo "Compiling..."
+javac $PACKAGE/Main.java
+echo "Running..."
+java $PACKAGE/Main
+echo "Done!"
+echo "Removing class files"
+rm -r $PACKAGE/*.class
+ls -al
 
 
 
